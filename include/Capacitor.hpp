8d7ccf94d44590529783e0c71b514e3bae53cd82
Sebastian Smith <sebastian.smith@gmail.com>
2015-06-26 17:38:42 -0700

    - Reverted capacitor, removed Terminal.hpp

--- include/Capacitor.hpp
+++ include/Capacitor.hpp
@@ -3,76 +3,37 @@
 
 #include <cmath>
 
-// TODO: Capacitor has invariants, should implement as class to enforce.
-class Capacitor 
+struct Capacitor 
 {
-public: 
-	struct CapacitorData
-	{
-		// Capacitance constants
-		double minCapacitance;
-		
-		// Current constants
-		double maxCurrent;
-		double leakageCurrent;
+	// Capacitance constants
+	double minCapacitance;
+	double meanCapacitance;
+	
+	// Current constants
+	double maxCurrent;
+	double leakageCurrent;
 
-		// Voltage constants
-		double ratedVoltage;
-		// TODO: Max voltage could be statistically modeled
-		double maxVoltage;
-				
-		// Variables
-		double esr;
-		// Capacitance varies non-linearly with voltage
-		double capacitance;
-		double maxCapacitance;
-		double voltage;
-		// Coefficient that adds non-linear effects due to diffused layer
-		double diffusedEffectsCoef;
-		double energy;
-
-		CapacitorData();
-	};
-
-private:
-	// Members
-	CapacitorData _data;
-
-protected:
-	// Methods
-	virtual void setMinCapacitance(double minCapacitance);
-	virtual void setMaxCurrent(double maxCurrent);
-	virtual void setLeakageCurrent(double leakageCurrent);
-	virtual void setRatedVoltage(double ratedVoltage);
-	virtual void setMaxVoltage(double maxVoltage);
-	virtual void setCapacitance(double capacitance);
-	virtual void setMaxCapacitance(double maxCapacitance);
-	virtual void setVoltage(double voltage);
-	virtual void setDiffusedEffectsCoef(double diffusedEffectsCoef);
-	virtual void setEnergy(double energy);
-	virtual void setEsr(double esr);
-
-public:
-	// Constructors
-	Capacitor();
-	Capacitor(const CapacitorData &capData);
-
-	// Methods
-	virtual double getMinCapacitance() const;
-	virtual double getMaxCurrent() const;
-	virtual double getLeakageCurrent() const;
-	virtual double getRatedVoltage() const;
-	virtual double getMaxVoltage() const;
-	virtual double getCapacitance() const;
-	virtual double getMaxCapacitance() const;
-	virtual double getVoltage() const;
-	virtual double getDiffusedEffectsCoef() const;
-	virtual double getEnergy() const;
-	virtual double getEsr() const;
-	virtual void update(const double current, const double time);
+	// Voltage constants
+	double ratedVoltage;
+	// Max voltage could be statistically modeled
+	double maxVoltage;
+	
+	// Resistance constants
+	double maxEsr;
+	double meanEsr;
+	
+	// Variables
+	double esr;
+	// Capacitance varies non-linearly with voltage
+	double capacitance;
+	double maxCapacitance;
+	double voltage;
+	// Coefficient that adds non-linear effects due to diffused layer
+	double diffusedEffectsCoef;
+	double energy;
 };
 
-class MaxwellK23400F 
+struct MaxwellK23400F 
 	: public Capacitor
 {
 	MaxwellK23400F(double startingVoltage = 0);
@@ -84,25 +45,20 @@ class MaxwellK23400F
 // return > 0 = discharge
 inline double calcCurrentFromPower(const Capacitor &cap, const double power, const double time)
 {
-	double voltage = cap.getVoltage();
-	double minCapacitance = cap.getMinCapacitance();
-	return -(power / voltage * sqrt(minCapacitance * voltage * voltage)) / \
-		(minCapacitance * voltage * voltage - 2 * power * time);
+	return -(power / cap.voltage) * sqrt(cap.minCapacitance * cap.voltage * cap.voltage) / \
+		(cap.minCapacitance * cap.voltage * cap.voltage - 2 * power * time);
 }
 
 // current < 0 = charge
 // current > 0 = discharge
 inline double calcVoltageDelta(const Capacitor &cap, const double current, const double time)
 {
-	double minCapacitance = cap.getMinCapacitance();
-	double diffusedCoef = cap.getDiffusedEffectsCoef();
-	double voltage = cap.getVoltage();
-	return sqrt((minCapacitance * minCapacitance) / \
-		(4 * diffusedCoef * diffusedCoef) + \
-		(1 / diffusedCoef) * \
-		(voltage * minCapacitance + \
-		voltage * voltage * diffusedCoef - current * time)) - \
-		minCapacitance / (2 * diffusedCoef); // Volts
+	return sqrt((cap.minCapacitance * cap.minCapacitance) / \
+		(4 * cap.diffusedEffectsCoef * cap.diffusedEffectsCoef) + \
+		(1 / cap.diffusedEffectsCoef) * \
+		(cap.voltage * cap.minCapacitance + \
+		cap.voltage * cap.voltage * cap.diffusedEffectsCoef - current * time)) - \
+		cap.minCapacitance / (2 * cap.diffusedEffectsCoef); // Volts
 
 	// Capacitor voltage can become negative, but should not be done for safety.
 }
@@ -111,15 +67,14 @@ inline double calcCapacitance(const Capacitor &cap)
 {
 	// C(u_c) = C_0 + k_c * u_c
 	// where k_c = diffused effects coefficient
-	return cap.getMinCapacitance() + cap.getDiffusedEffectsCoef() * cap.getVoltage(); // Farads
+	return cap.minCapacitance + cap.diffusedEffectsCoef * cap.voltage; // Farads
 }
 
 inline double calcEnergy(const Capacitor &cap)
 {
 	// "Energetic" capacitance = C_e(u_c) = C_0 + 4/3 * k_c * u_c
-	double voltage = cap.getVoltage();
-	return 0.5 * (cap.getMinCapacitance() + 4 / 3 * cap.getDiffusedEffectsCoef() * voltage) * \
-		voltage * voltage; // Joules
+	return 0.5 * (cap.minCapacitance + 4 / 3 * cap.diffusedEffectsCoef * cap.voltage) * \
+		cap.voltage * cap.voltage; // Joules
 }
 
 #endif
--- src/Capacitor.cc
+++ src/Capacitor.cc
@@ -1,4 +1,3 @@
-#include <cassert>
 #include <chrono>
 #include <cmath>
 #include <random>
@@ -6,226 +5,57 @@
 #include <stdexcept>
 #include "Capacitor.hpp"
 
-Capacitor::CapacitorData::CapacitorData()
-{
-	minCapacitance = 0.0;
-	maxCurrent = 0.0;
-	leakageCurrent = 0.0;
-	ratedVoltage = 0.0;
-	maxVoltage = 0.0;
-	esr = 0.0;
-	capacitance = 0.0;
-	maxCapacitance = 0.0;
-	voltage = 0.0;
-	diffusedEffectsCoef = 0.0;
-	energy = 0.0;
-}
-
-Capacitor::Capacitor()
-{
-	
-}
-
-Capacitor::Capacitor(const CapacitorData &capData)
-{
-	// Satisify class invariants -- check input capacitor data values.
-	setMinCapacitance(capData.minCapacitance);
-	setMaxCurrent(capData.maxCurrent);
-	setLeakageCurrent(capData.leakageCurrent);
-	setRatedVoltage(capData.ratedVoltage);
-	setMaxVoltage(capData.maxVoltage);
-	setEsr(capData.esr);
-	setCapacitance(capData.capacitance);
-	setMaxCapacitance(capData.maxCapacitance);
-	setVoltage(capData.voltage);
-	setDiffusedEffectsCoef(capData.diffusedEffectsCoef);
-
-	// Set capacitance and energy based on initial parameters
-	setCapacitance(calcCapacitance(*this));
-	setEnergy(calcEnergy(*this));
-}
-
-double Capacitor::getMinCapacitance() const
-{
-	return _data.minCapacitance;
-}
-
-void Capacitor::setMinCapacitance(double minCapacitance)
-{
-	assert(minCapacitance > 0);
-	_data.minCapacitance = minCapacitance;
-}
-
-double Capacitor::getMaxCurrent() const
-{
-	return _data.maxCurrent;
-}
-
-void Capacitor::setMaxCurrent(double maxCurrent)
-{
-	assert(maxCurrent > 0);
-	_data.maxCurrent = maxCurrent;
-}
-
-double Capacitor::getLeakageCurrent() const
-{
-	return _data.leakageCurrent;
-}
-
-void Capacitor::setLeakageCurrent(double leakageCurrent)
-{
-	assert(leakageCurrent > 0);
-	_data.leakageCurrent = leakageCurrent;
-}
-
-double Capacitor::getRatedVoltage() const
-{
-	return _data.ratedVoltage;
-}
-
-void Capacitor::setRatedVoltage(double ratedVoltage)
-{
-	assert(ratedVoltage > 0);
-	_data.ratedVoltage = ratedVoltage;
-}
-
-double Capacitor::getMaxVoltage() const
-{
-	return _data.maxVoltage;
-}
-
-void Capacitor::setMaxVoltage(double maxVoltage)
-{
-	assert(maxVoltage > 0);
-	_data.maxVoltage = maxVoltage;
-}
-
-double Capacitor::getCapacitance() const
-{
-	return _data.capacitance;
-}
-
-void Capacitor::setCapacitance(double capacitance)
-{
-	assert(capacitance > 0);
-	_data.capacitance = capacitance;
-}
-
-double Capacitor::getMaxCapacitance() const
-{
-	return _data.maxCapacitance;
-}
-
-void Capacitor::setMaxCapacitance(double maxCapacitance)
-{
-	assert(maxCapacitance > 0);
-	_data.maxCapacitance = maxCapacitance;
-}
-
-double Capacitor::getVoltage() const
-{
-	return _data.voltage;
-}
-
-void Capacitor::setVoltage(double voltage)
-{
-	// Voltage may be negative
-	_data.voltage = voltage;
-}
-
-double Capacitor::getDiffusedEffectsCoef() const
-{
-	return _data.diffusedEffectsCoef;
-}
-
-void Capacitor::setDiffusedEffectsCoef(double diffusedEffectsCoef)
-{
-	assert(diffusedEffectsCoef > 0);
-	_data.diffusedEffectsCoef = diffusedEffectsCoef;
-}
-
-double Capacitor::getEnergy() const
-{
-	return _data.energy;
-}
-
-void Capacitor::setEnergy(double energy)
-{
-	// Energy could be negative
-	_data.energy = energy;
-}
-
-double Capacitor::getEsr() const
-{
-	return _data.esr;
-}
-
-void Capacitor::setEsr(double esr)
-{
-	assert(esr > 0);
-	_data.esr = esr;
-}
-
-void Capacitor::update(const double current, const double time)
-{
-	setVoltage(calcVoltageDelta(*this, current, time));
-	setCapacitance(calcCapacitance(*this));
-	setEnergy(calcEnergy(*this));
-}
-
 MaxwellK23400F::MaxwellK23400F(double startingVoltage) 
 {
 	// Set voltage
-	setRatedVoltage(2.85); // Volts
-	setMaxVoltage(3.0); // Volts
-	// Starting voltage should be between 0..maxVoltage.
-	assert(startingVoltage > 0 || startingVoltage < getMaxVoltage());
-	// if (startingVoltage < 0 || startingVoltage > maxVoltage) 
-	// {
-	// 	std::ostringstream message;
-	// 	message << "Starting voltage is out of range 0.." << maxVoltage << "V.";
-	// 	throw std::out_of_range (message.str());
-	// }
-	setVoltage(startingVoltage);
+	ratedVoltage = 2.85; // Volts
+	maxVoltage = 3.0; // Volts
+	if (startingVoltage < 0 || startingVoltage > maxVoltage) 
+	{
+		std::ostringstream message;
+		message << "Starting voltage is out of range 0.." << maxVoltage << "V.";
+		throw std::out_of_range (message.str());
+	}
+	voltage = startingVoltage;
 
 	// Set diffused coefficient
 	// This value should be measured empirically for each capacitor.
 	// Using model from "Ultra-Capacitors in power conversion systems" for 3000F/2.5V Boostcap
-	setDiffusedEffectsCoef(473.0); // Farads/Volt
+	diffusedEffectsCoef = 473.0; // Farads/Volt
 
 	// Set capacitance
 	// Datasheet min capacitance will be used for calculating a statistical max capacitance.
 	// Statistical max will, in turn, be used to calc a new minimum based on diffused effects
 	// coefficient.
-	setMinCapacitance(3400.0); // Farad
-	double meanCapacitance = 3550.0; // Farad
+	minCapacitance = 3400.0; // Farad
+	meanCapacitance = 3550.0; // Farad
 	// Difference between mean and min is 150F. Selecting this as 3*sigma.
 	// Sigma = 50F. Should establish sigma empirically.
 	// Generate random capacitance value.
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator (seed);
 	std::normal_distribution<double> capDist (meanCapacitance, 
-		(meanCapacitance - getMinCapacitance()) / 3.0);
-	double maxCapacitance = capDist(generator); // Farad
+		(meanCapacitance - minCapacitance) / 3.0);
+	maxCapacitance = capDist(generator); // Farad
 	// Calculate minimum capacitance from diffused effects coefficient and max voltage.
 	// C(u_c) = C_0 + k_c * u_c
 	// C_0 = C(max_volt) - k_c * max_volt
 	// where k_c = diffused effects coefficient
-	setMinCapacitance(maxCapacitance - getDiffusedEffectsCoef() * getMaxVoltage());
-	setCapacitance(calcCapacitance(*this));
+	minCapacitance = maxCapacitance - diffusedEffectsCoef * maxVoltage;
+	calcCapacitance(*this);
 
 	// Set current
-	setMaxCurrent(2000.0); // Amp
-	setLeakageCurrent(0.018); // Amp
+	maxCurrent = 2000.0; // Amp
+	leakageCurrent = 0.018; // Amp
 
 	// Set resistance
-	double maxEsr = 0.00028; // Ohms
-	double meanEsr = 0.00022; // Ohms
+	maxEsr = 0.00028; // Ohms
+	meanEsr = 0.00022; // Ohms
 	// Difference between mean and min is 0.00006 Ohms. Selecting this as
 	// sigma.
 	// Generate random ESR value.
-	std::normal_distribution<double> esrDist (meanEsr, maxEsr - meanEsr);
-	setEsr(esrDist(generator)); // Ohms
+	std::normal_distribution<double> esrDist (meanEsr, 6e-6);
+	esr = esrDist(generator); // Ohms
 
 	// Set energy
 	calcEnergy(*this);
